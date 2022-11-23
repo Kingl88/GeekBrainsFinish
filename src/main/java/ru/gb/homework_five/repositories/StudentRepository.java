768a32067bbc9e90061bdd7626eec5e558bc2369
Kingl <siarheitsarik@gmail.com>
2022-11-24 01:44:35 +0300

    Homework Five

---
+++ src/main/java/ru/gb/homework_five/repositories/StudentRepository.java
@@ -0,0 +1,7 @@
+package ru.gb.homework_five.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.gb.homework_five.entyties.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
